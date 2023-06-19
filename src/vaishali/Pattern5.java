48e789fa9eecebd0b4532130d4c1ef659d8454bd
vaish <vaish@DESKTOP-MIOSD5A>
2023-06-19 21:20:42 +0530

    diamond and other pattern

---
+++ src/vaishali/Pattern5.java
@@ -0,0 +1,14 @@
+package vaishali;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for (int i = 1; i <= 4; i++) {
+	            // inner loop to handle number of columns
+	            for (int j = 1; j <= i; j++) {
+	                // printing value of num in each iteration.
+	                System.out.print("*");
+	}
+System.out.println();
+}}}
---
+++ src/vaishali/Pattern6.java
@@ -0,0 +1,16 @@
+package vaishali;
+
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for (int i = 1; i <= 4; i++) {
+	            // inner loop to handle number of columns
+	            for (int j = 4; j >= i; j--) {
+	             
+	                System.out.print("*");
+	}
+System.out.println();
+	}
+	}
+}
