25ee7880720d59fe2baec6471c4ef6fdadf8dac3
vaish <vaish@DESKTOP-MIOSD5A>
2023-06-19 21:36:02 +0530

    right pascal

---
+++ src/vaishali/RightPascals.java
@@ -0,0 +1,30 @@
+package vaishali;
+import java.util.*;
+public class RightPascals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j, rows;  
+		Scanner sc = new Scanner(System.in);  
+		System.out.print("Enter the number of rows you want to print: ");  
+		rows = sc.nextInt();     
+		
+		for (  i= 0; i<= rows-1; i++)  
+		{  
+		for (  j=0; j<=i; j++)   
+		{  
+		System.out.print("*"+ " ");  
+		}   
+		System.out.println("");   
+		}   
+		for (  i=rows-1; i>=0; i--)  
+		{  
+		for(  j=0; j <= i-1;j++)  
+		{  
+		System.out.print("*"+ " ");  
+		}  
+		System.out.println("");  
+		}  
+	}
+
+}
