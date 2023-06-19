a52a5cc99780b78fc5039a71c91d849316521edd
vaish <vaish@DESKTOP-MIOSD5A>
2023-06-19 20:27:42 +0530

    pattern1

---
+++ src/vaishali/Pattern1.java
@@ -0,0 +1,15 @@
+package vaishali;
+
+public class Pattern1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=3;i++) //outer loop for no of rows
+		 {
+		 for(int j=1;j<=5;j++) //inner loop for no of columns 
+		 {
+		 System.out.print("*");
+		 }
+		 System.out.println(" ");
+	}
+
+}}
